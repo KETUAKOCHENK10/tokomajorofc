d7751caa27610b87a04e1ffed329dc6deae5608f
KETUAKOCHENK10 <113256470+KETUAKOCHENK10@users.noreply.github.com>
2023-03-12 07:18:01 +0700

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -52,6 +52,7 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4 );
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 //curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
