78721cdc45901d2244df3b91c8267300d4a3a35b
KETUAKOCHENK10 <113256470+KETUAKOCHENK10@users.noreply.github.com>
2023-03-15 11:18:47 +0700

    Add files via upload

--- follow.php
+++ follow.php
@@ -1,11 +1,14 @@
 <?php
 system("clear");
 include"proxy.php";
-error_reporting(0);
-function device($r32,$r15,$r16){
-return curl(["Host: gw-napi.zepeto.io","x-zepeto-duid: ".$r32,"user-agent: android.zepeto_global/3.22.200 (android; U; Android OS 8.1.0 / API-27 (O11019/9f4ea240fa3f5f83); id-ID; occ-ID; ".$r15." ".$r16.")","content-type: application/json; charset=utf-8"],'https://gw-napi.zepeto.io/DeviceAuthenticationRequest','{"deviceId":"'.$r32.'"}');
-}
 
+//error_reporting(0);
+function device($r32,$r15,$r16){
+$res=curl(["Host: gw-napi.zepeto.io","x-zepeto-duid: ".$r32,"user-agent: android.zepeto_global/3.22.200 (android; U; Android OS 8.1.0 / API-27 (O11019/9f4ea240fa3f5f83); id-ID; occ-ID; ".$r15." ".$r16.")","content-type: application/json; charset=utf-8"],'https://gw-napi.zepeto.io/DeviceAuthenticationRequest','{"deviceId":"'.$r32.'"}');
+$js=json_decode($res,true);
+$auth=$js["authToken"];
+return $auth;
+}
 function save($auth,$uuid,$ua1,$ua2,$nama){
 return curl(["Host: gw-napi.zepeto.io","authorization: Bearer $auth","x-zepeto-duid: $uuid","user-agent: android.zepeto_global/3.22.200 (android; U; Android OS 8.1.0 / API-27 (O11019/9f4ea240fa3f5f83); id-ID; occ-ID; ".$ua1." ".$ua2.")","x-timezone: Asia/Jakarta","content-type: application/json; charset=utf-8"],'https://gw-napi.zepeto.io/SaveProfileRequest_v2','{"job":"","name":"'.$nama.'","nationality":"","statusMessage":""}');
 }
@@ -16,12 +19,11 @@ function follow($auth,$uuid,$ua1,$ua2,$target,$nama){
 global $hijau,$off;
 $request=curl(["Host: gw-napi.zepeto.io","authorization: Bearer $auth","x-zepeto-duid: $uuid","user-agent: android.zepeto_global/3.22.200 (android; U; Android OS 8.1.0 / API-27 (O11019/9f4ea240fa3f5f83); id-ID; occ-ID; ".$ua1." ".$ua2.")","x-timezone: Asia/Jakarta","content-type: application/json; charset=utf-8"],'https://gw-napi.zepeto.io/FollowRequest_v2','{"followUserId":"'.$target.'"}');
 if(json_decode($request,true)["isSuccess"] == "true"){
-echo "{$hijau}$nama{$off} success\n";
+echo "$nama success\n";
 }else{
 echo " follow gagal,unknown error\n";
 }
 }
-
 function send($auth,$uuid,$ua1,$ua2,$email){
 return curl(["Host: gw-napi.zepeto.io","authorization: Bearer $auth","x-zepeto-duid: $uuid","user-agent: android.zepeto_global/3.22.200 (android; U; Android OS 8.1.0 / API-27 (O11019/9f4ea240fa3f5f83); id-ID; occ-ID; ".$ua1." ".$ua2.")","x-timezone: Asia/Jakarta","content-type: application/json; charset=utf-8"],'https://gw-napi.zepeto.io/EmailVerificationRequest','{"email":"'.$email.'"}');
 }
@@ -34,40 +36,28 @@ return curl(["Host: gw-napi.zepeto.io","authorization: Bearer $auth","x-zepeto-d
 function reg($auth,$uuid,$ua1,$ua2,$email){
 return curl(["Host: gw-napi.zepeto.io","authorization: Bearer $auth","x-zepeto-duid: $uuid","user-agent: android.zepeto_global/3.22.200 (android; U; Android OS 8.1.0 / API-27 (O11019/9f4ea240fa3f5f83); id-ID; occ-ID; ".$ua1." ".$ua2.")","x-timezone: Asia/Jakarta","content-type: application/json; charset=utf-8"],'https://gw-napi.zepeto.io/AuthenticationRequest_v2','{"userId":"'.$email.'","password":"Dua781996"}');
 }
-function search($auth,$uuid,$ua1,$ua2){
-$nama=readline("\nnickname: ");
-system("clear");
-echo "searching account name $nama\n\n";
-$request=curl(["Host: gw-napi.zepeto.io","authorization: Bearer $auth","x-zepeto-duid: $uuid","user-agent: android.zepeto_global/3.22.200 (android; U; Android OS 8.1.0 / API-27 (O11019/9f4ea240fa3f5f83); id-ID; occ-ID; ".$ua1." ".$ua2.")","x-timezone: Asia/Jakarta","content-type: application/json; charset=utf-8"],'https://gw-napi.zepeto.io/FriendNicknameSearchWithFeedInfo','{"cursor":"0","keyword":"'.$nama.'","size":30,"place":"home"}');
-for($id=0;$id>=0;$id++){
-$js=json_decode($request,true);
-$userid=$js["result"][$id]["userId"];
-$nama=$js["result"][$id]["name"];
-$follower=$js["result"][$id]["followerCount"];
-$post=$js["result"][$id]["postCount"];
-if($nama==null){
-break;
+function ceklimit($uid,$auth,$uuid,$ua1,$ua2){
+$req=curl(["Host: gw-napi.zepeto.io","authorization: Bearer $auth","x-zepeto-duid: $uuid","user-agent: android.zepeto_global/3.22.200 (android; U; Android OS 8.1.0 / API-27 (O11019/9f4ea240fa3f5f83); id-ID; occ-ID; ".$ua1." ".$ua2.")","x-timezone: Asia/Jakarta","content-type: application/json; charset=utf-8"],'https://gw-napi.zepeto.io/UserVisitRequest_v2','{"visitUserId":"'.$uid.'","resolve":true}');
+return ex('"followerCount":',',',1,$req);
 }
-echo "nama     : $nama\n";
-echo "follower : $follower\n";
-echo "jml post : $post\n";
-echo "userid   : $userid\n\n";
-}
-}
+
 
 
 $target=$argv[1];
+$limit=$argv[2];
+
 while(true){
 $uuid=random(32);
 $ua1=random(15);
 $ua2=random(16);
-$get_device=device($uuid,$ua1,$ua2);
-$js=json_decode($get_device,true);
-$auth=$js["authToken"];
-$userid=$js["userId"];
-$name=nama();
+$auth=device($uuid,$ua1,$ua2);
+$foll=ceklimit($target,$auth,$uuid,$ua1,$ua2);
+if($foll >= $limit){
+echo "$hijau success mencapai target\n";
+exit;
+}
+$name=nama($uuid,$ua1,$ua2);
 $save=save($auth,$uuid,$ua1,$ua2,$name);
 $save=saveCharacter($auth,$uuid,$ua1,$ua2);
-
 $follow=follow($auth,$uuid,$ua1,$ua2,$target,$name);
 }
